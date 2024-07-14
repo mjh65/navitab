34c76857f0eb8e91a74d1e643d0da536f23e7c8a
Michael Hasling <mjh65@github.com>
2024-07-14 16:13:04 +0100

    Basic win32 desktop app framework now in place.
    Stoopid extra while(1) loop, now fixed!
    Added some install targets to start building install packages.

--- apps/desktop/win32main.cpp
+++ apps/desktop/win32main.cpp
@@ -28,13 +28,12 @@
 #include <memory>
 #include "navitab/core.h"
 
-// Forward declarations of local functions
+// Forward declarations of local functions and shared data
 LRESULT CALLBACK WindowProc(HWND h, UINT msg, WPARAM wp, LPARAM lp);
-
-static LPCWSTR WINDOW_CLASS = TEXT("NavitabWindow");
 
 int WINAPI WinMain(_In_ HINSTANCE hinst, _In_opt_ HINSTANCE hprev, _In_ LPSTR cmdline, _In_ int show)
 {
+    const static LPCWSTR WINDOW_CLASS = TEXT("NavitabWindow");
     WNDCLASS c = { 0 };
     c.lpfnWndProc = WindowProc;
     c.hInstance = hinst;
@@ -62,58 +61,64 @@ int WINAPI WinMain(_In_ HINSTANCE hinst, _In_opt_ HINSTANCE hprev, _In_ LPSTR cm
 
     ShowWindow(h, show);
 
-    while (1)
+    MSG msg = { };
+    while (GetMessage(&msg, NULL, 0, 0))
     {
-        MSG msg = { };
-        while (GetMessage(&msg, NULL, 0, 0) > 0)
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
     }
+
     return 0;
 }
-
-
-
 
 LRESULT CALLBACK WindowProc(HWND h, UINT msg, WPARAM wp, LPARAM lp)
 {
     switch (msg)
     {
-#if 0
-    case WM_CREATE:
+    default:
+        return DefWindowProc(h, msg, wp, lp);
 
-        {
-        HWND hbutton = CreateWindowEx("BUTTON", "Hey There",  /* class and title */
-            WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, /* style */
-            0, 0, 100, 30,            /* position */
-            h,                     /* parent */
-            (HMENU)101,            /* unique (within the application) integer identifier */
-            GetModuleHandle(0), 0   /* GetModuleHandle(0) gets the hinst */
-        );
-    }
-                  break;
-
-    case WM_COMMAND:
-        switch (LOWORD(wp)) {
-        case 101: /* the unique identifier used above. These are usually #define's */
-            PostQuitMessage(1);
-            break;
-        default:;
-        }
-        break;
-
-    case WM_CLOSE:
-        PostQuitMessage(1);
-        break;
-#endif
     case WM_DESTROY:
         PostQuitMessage(1);
         return 0;
 
-    default:
+#if 0
+    // these are messages we've seen during debugging, we might want to handle some of them?
+    case WM_CREATE:             // 1
+    case WM_MOVE:               // 3
+    case WM_SIZE:               // 5
+    case WM_ACTIVATE:           // 6
+    case WM_SETFOCUS:           // 7
+    case WM_KILLFOCUS:          // 8
+    case WM_PAINT:              // 15
+    case WM_CLOSE:              // 16
+    case WM_ERASEBKGND:         // 20
+    case WM_SHOWWINDOW:         // 24
+    case WM_ACTIVATEAPP:        // 28
+    case WM_SETCURSOR:          // 32
+    case WM_GETMINMAXINFO:      // 36
+    case WM_WINDOWPOSCHANGING:  // 70
+    case WM_WINDOWPOSCHANGED:   // 71
+    case WM_GETICON:            // 127
+    case WM_NCCREATE:           // 129
+    case WM_NCDESTROY:          // 130
+    case WM_NCCALCSIZE:         // 131
+    case WM_NCHITTEST:          // 132
+    case WM_NCPAINT:            // 133
+    case WM_NCACTIVATE:         // 134
+    case 0x90:                  // 144 - ????
+    case WM_NCMOUSEMOVE:        // 160
+    case WM_NCLBUTTONDOWN:      // 161
+    case WM_SYSCOMMAND:         // 274
+    case WM_MOUSEMOVE:          // 512
+    case WM_CAPTURECHANGED:     // 533
+    case WM_IME_SETCONTEXT:     // 641
+    case WM_IME_NOTIFY:         // 642
+    case WM_IME_REQUEST:        // 648
+    case WM_NCMOUSELEAVE:       // 674
+    case WM_DWMNCRENDERINGCHANGED:  // 799=0x31f
         return DefWindowProc(h, msg, wp, lp);
+#endif
     }
 
     return 0;

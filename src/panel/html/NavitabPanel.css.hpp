58b6506c438c7b0f778f02813c2cbd1c5816ba7e
MJH <mjh65@github.com>
2024-08-25 13:08:26 +0100

    Add support for an html panel running on a local server (Servez recomended).

---
+++ src/panel/html/NavitabPanel.css.hpp
@@ -0,0 +1,23 @@
+﻿aviators-tablet #NavitabPanel {
+    width: 100%;
+    height: 100%;
+    min-width: 160;
+    min-height: 90;
+    display:flex;
+}
+aviators-tablet #NavitabDisplay {
+    display: flex;
+    width: 100%;
+    height: 100%;
+}
+aviators-tablet #NavitabCanvas {
+    display: block;
+}
+aviators-tablet #ScratchArea {
+    display: none;
+    width: 100%;
+    height: 0%;
+}
+aviators-tablet #ImageBuffer {
+    display: none;
+}
